70c5987a42fd9a8a5d0dfe4e865cf1182ee9bcfb
genx-1 <156506319+genx-1@users.noreply.github.com>
2024-11-19 19:01:58 +0530

    Update Account.java
    added code in account class

--- BasicjavaPrograme/src/encapsulation/Account.java
+++ BasicjavaPrograme/src/encapsulation/Account.java
@@ -20,6 +20,23 @@ public class Account {
 	         * "CheckBalance" method provided by the "Account" class 
 	         * but it cannot manipulate the value of "accountBalance" */
 
+		     private int _accountBalance = 100;
+
+	        public int CheckBalance()
+	        {
+	            return _accountBalance;
+	        }
+	    
+
+	    static void Main()
+	    {
+	        Account myAccount = new Account();
+	       int myBalance = myAccount.CheckBalance();
+
+	        /* This Main method can check the balance via the public
+	         * "CheckBalance" method provided by the "Account" class 
+	         * but it cannot manipulate the value of "accountBalance" */
+
 	}
 
 }
